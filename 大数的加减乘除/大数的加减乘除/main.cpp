5d54215e6ec51b440246adb20bb388f8028098fe
huanglei <shixi_huanglei@staff.sina.com.cn>
2015-10-24 12:43:23 +0800

    大数的加减乘除

--- 大数的加减乘除/大数的加减乘除/main.cpp
+++ 大数的加减乘除/大数的加减乘除/main.cpp
@@ -1,8 +1,8 @@
 //
 //  main.cpp
-//  大数（正整数）的加减乘除
+//  大数的加减乘除
 //
-//  Created by Lei Huang on 15/9/18.
+//  Created by Lei Huang on 15/10/24.
 //  Copyright (c) 2015年 LeiHuang. All rights reserved.
 //
 
@@ -12,22 +12,52 @@
 using namespace std;
 
 const int digit = 1000; //定义最大位数为1000位
-int size_result=0,size_ans=0;
+int size_result=0,size_ans=0;//记录result[]和ans[]的位数
 
-//void separate(int ans[], int number)
-//{
-//    for (int i = 0; i<digit; i++) {
-//        ans[i]=0;
-//    }
-//    size_ans=0;
-//    while (number!=0) {
-//        ans[size_ans++]=number%10;
-//        number=number/10;
-//    }
-//}
+void separate(int ans[], int number);
+void separate(int ans[], string str);
+void add(int result[],int ans[]);
+void out(int result[]);
+
+int main(int argc, const char * argv[]) {
+    //如果数输入时超过最大正整数，则用 str[ ] 读取，否则用 number[ ]
+    // int number[2];
+    string str[2];
+    
+    int result[digit],ans[digit];
+    
+    for (int i = 0; i<digit; i++) {
+        result[i]=0;ans[i]=0;     //初始化
+    }
+    //大数的加法
+    for (int i = 0; i<2; i++) {
+        //cin>>number[i];
+        cin>>str[i];
+        //separate(ans, number[i]);
+        separate(ans, str[i]);
+        add(result, ans);
+    }
+    out(result);
+    return 0;
+}
+
+
+void separate(int ans[], int number)
+{
+    //把number内的数字分割存入ans[]中
+    for (int i = 0; i<digit; i++) {
+        ans[i]=0;
+    }
+    size_ans=0;
+    while (number!=0) {
+        ans[size_ans++]=number%10;
+        number=number/10;
+    }
+}
 
 void separate(int ans[], string str)
 {
+    //把str内的数字分割存入ans[]中
     for (int i = 0; i<digit; i++) {
         ans[i]=0;
     }
@@ -41,9 +71,9 @@ void separate(int ans[], string str)
     }
 }
 
-
 void add(int result[],int ans[])
 {
+    //将ans[]与result[]相加，存入result[]中
     int carryDigit=0;//进位数
     int remainder=0;//余数
     size_result=0;
@@ -72,6 +102,7 @@ void add(int result[],int ans[])
 
 void out(int result[])
 {
+    //输出
     int k=digit-1;
     while (result[k]==0) {
         k--;
@@ -80,28 +111,4 @@ void out(int result[])
         cout<<result[k--];
     }
 }
-
-int main(int argc, const char * argv[]) {
-    //如果数输入时超过最大正整数，则用 str[ ] 读取，否则用 number[ ]
-    
-   // int number[2];
-    string str[2];
-    
-    int result[digit],ans[digit];
-    
-    for (int i = 0; i<digit; i++) {
-        result[i]=0;ans[i]=0;     //初始化
-    }
-    //大数的加法
-    for (int i = 0; i<2; i++) {
-        //cin>>number[i];
-        cin>>str[i];
-        //separate(ans, number[i]);
-        separate(ans, str[i]);
-        add(result, ans);
-    }
-    out(result);
-    return 0;
-}
-
 
